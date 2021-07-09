b36ab9b93f933837aeea172451d218c4b7f3a2be
Alexandr <torasov.alexandr@gmail.com>
2021-07-09 06:27:53 +0300

    FirstPush

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import "./App.css";
+// import { useRoutes } from "./routes";
+
+// import { Navbar } from "./components/navBar/navBar";
+// import { Footer } from "./components/footer/footer";
+
+function App() {
+  return <main>sadad</main>;
+}
+export default App;
