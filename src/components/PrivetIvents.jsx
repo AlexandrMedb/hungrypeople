4f9250055978fc5123a27138c9013b8ad3400be0
AlexandrTarasov <torasov.alexandr@gmail.com>
2021-07-20 19:19:43 +0300

    goldSquare

--- src/components/PrivetIvents.jsx
+++ src/components/PrivetIvents.jsx
@@ -7,22 +7,13 @@ export const PrivetIvents = () => {
       <div className=" PrivetIvents">
         <h3>PRIVATE EVENTS</h3>
         <div className=" PrivetIvents__main">
-          <div className="left"></div>
+          <div className="left">
+            <div className="square"></div>
+            <p>weddings</p>
+          </div>
           <div className="right">
-            <div className="title">
-              <h2>chocolate pancakes</h2>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis at
-                velit maximus, molestie est a, tempor magna.
-              </p>
-            </div>
-            <p>
-              integer ullamcorper neque eu purus euismod, ac faucibus mauris
-              posuere. Morbi non ultrices ligula. Sed dictum, enim sed
-              ullamcorper feugiat, dui odio vehicula eros, a sollicitudin lorem
-              quam nec sem. Mauris tincidunt feugiat diam convallis pharetra.
-              Nulla facilisis semper laoreet.
-            </p>
+            <p>weddings</p>
+            <div className="square"></div>
           </div>
         </div>
         <p>
---
+++ src/components/Specialites.jsx
@@ -0,0 +1,15 @@
+<div className="right">
+<div className="title">
+  <h2>chocolate pancakes</h2>
+  <p>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis at
+    velit maximus, molestie est a, tempor magna.
+  </p>
+</div>
+<p>
+  integer ullamcorper neque eu purus euismod, ac faucibus mauris
+  posuere. Morbi non ultrices ligula. Sed dictum, enim sed
+  ullamcorper feugiat, dui odio vehicula eros, a sollicitudin lorem
+  quam nec sem. Mauris tincidunt feugiat diam convallis pharetra.
+  Nulla facilisis semper laoreet.
+</p>
